f929b6865f0ede4c7146db34775dc0043d5a64ea
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 01:17:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+array = ["kiwi"]
 def oxford_comma(array)
-
+  if array.length == 2
+  array.join (" and ")
+  elsif array.length == 1
+  	array.join
+  else str = array.pop
+	array.join(", ") << ", and #{str}"
+  end
 end
